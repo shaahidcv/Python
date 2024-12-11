51fb15589a4e1472973715535081c3dbf8e8a897
shaahidcv <shaahid.dev@gmail.com>
2024-12-11 11:49:39 +0530

    changes made

---
+++ logic.py
@@ -0,0 +1,28 @@
+#NOTE: Patterns for logic building :
+#pattern problems:
+
+#   1) square star pattern
+for i in range(4):
+
+    for j in range(4):
+        print("#",end="")
+
+    print()
+
+
+# 2) inverted pyramid star pattern:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
