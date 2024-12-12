3c47842126588170bf46f7820c13764455325839
shaahidcv <shaahid.dev@gmail.com>
2024-12-12 14:39:23 +0530

    new pyrmid

--- patterns.py
+++ patterns.py
@@ -1,10 +1,20 @@
-# half pyramid star
+# left side half pyramid star
 num=5
 for i in range(1,num+1,1):
-    for j in range(0,i,1):
+    for j in range(1,i+1,1):
         print("*",end="")
     print()
 
 
-# inverted half pyramid star
+
+
+# right side half pyramid star 
+
+for i in range(1,num+1):
+    for j in range(0,num-i,1):
+        print(" ",end="")
+    for j in range(0,i):
+        print("*",end="")
+    print()
+
 
